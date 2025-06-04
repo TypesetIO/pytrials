f242b538e062bd254b3cbf77fe33d657fd92ccef
Rohan Tondulkar <106648681+RohanTondulkar@users.noreply.github.com>
2025-06-04 14:07:48 +0530

    Update utils.py to replace requests with urllib

--- pytrials/utils.py
+++ pytrials/utils.py
@@ -2,26 +2,26 @@
 import requests
 import csv
 import re
+import json
 
 
 def request_ct(url):
     """Performs a get request that provides a (somewhat) useful error message."""
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.HTTPError as ex:
+        import urllib.request
+        response = urllib.request.urlopen(url)
+        return response
+    except urllib.error.HTTPError as ex:
         raise ex
     except ImportError:
         raise ImportError(
             "Couldn't retrieve the data, check your search expression or try again later."
         )
-    else:
-        return response
-
 
 def json_handler(url):
     """Returns request in JSON (dict) format"""
-    return request_ct(url).json()
+    response = request_ct(url)
+    return json.loads(response.read().decode('utf-8'))
 
 
 def csv_handler(url):
